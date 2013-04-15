db5d3c70bb391d6599a467a64fe78a560c0918c7
Steve McHail <mchail@gmail.com>
2013-04-14 20:22:55 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenhitbliss::Application.config.secret_token = '04a9eafb4f1950afad521d788399768539948046f9ae0a4bb434fb138bee7dfc9a3e04fd995a9e05bb41f145e3446f8c041c25e410f3593c055ccf6519b5466b'
